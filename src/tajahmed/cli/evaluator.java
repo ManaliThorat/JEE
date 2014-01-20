5187f83ee96f5801fdc8a007ae7d4dbee6f49f70
Taj Ahmed <tajahmed@thoughtworks.com>
2014-01-20 18:05:28 +0530

    build xml maintained

--- src/tajahmed/cli/evaluator.java
+++ src/tajahmed/cli/evaluator.java
@@ -2,7 +2,7 @@ package tajahmed.cli;
 
 import tajahmed.Evaluator.Evaluator;
 
-public class cli {
+public class evaluator {
     public static void main(String[] args) {
         Evaluator ev = new Evaluator();
         System.out.println(ev.Evaluate(args[0]));
